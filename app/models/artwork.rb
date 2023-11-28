27506d022897256ea0bf701c15f0a2ed4206ffd7
George Cook <georgehhcook@gmail.com>
2023-11-28 11:50:33 +0000

    all models/validations done

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,8 @@
 class Artwork < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates title, presence: true
+  validates description, presence: true
+  validates price_per_day, presence: true
+  validates category, presence: true
 end
