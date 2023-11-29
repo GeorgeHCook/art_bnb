62e5909faae0e8c08d79d1c630dc5b3b3098cce6
George Cook <georgehhcook@gmail.com>
2023-11-29 11:50:27 +0000

    added index and show, both working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,23 @@
 class BookingsController < ApplicationController
-  def new_booking_request
+  def new
     @artwork = Artwork.find(params[:artwork_id])
-    @booking_request = BookingRequest.new
+    @booking = Booking.new
   end
 
-  def create_booking_request
+  def create
     @artwork = Artwork.find(params[:artwork_id])
-    @booking_request = @artwork.booking_requests.build(booking_request_params)
-    @booking_request.user = current_user
-    if @booking_request.save
+    @booking = @artwork.booking.build(booking_params)
+    @booking.user = current_user
+    if @booking.save
       redirect_to @artwork, notice: 'Booking request sent.'
     else
-      render :new_booking_request
+      render :new
     end
   end
 
   private
 
-  def booking_request_params
-    params.require(:booking_request).permit(:start_date, :end_date, :message)
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
