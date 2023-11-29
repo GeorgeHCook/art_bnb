be92b6725a25f320e4b42e0cf7a772cc9cd39074
Hannah Brett <hannahebrett89@icloud.com>
2023-11-29 11:34:01 +0000

    Committing so I can pull

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,5 @@
 class ArtworksController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [:index, :show]
   # GET /artworks or /artworks.json
   def index
     @artworks = Artwork.all.limit(10) #limits index to first ten images
