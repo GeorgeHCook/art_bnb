d36a16b4c91934664c75b23d5d498704c5c0b087
George Cook <georgehhcook@gmail.com>
2023-11-29 10:49:48 +0000

    route and controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :artworks
+  # resources :artworks
   devise_for :users
   root to: "pages#home"
 
-  resources :artworks, only: [:index, :show, :edit, :update]
+  resources :artworks, only: [:index, :show]
   resources :bookings, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
