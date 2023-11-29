a408d525975d1b5bb5ae3fe29845b4edc4245813
George Cook <georgehhcook@gmail.com>
2023-11-29 13:42:55 +0000

    dashboard linked to current user attributes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :dashboard]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
 
   def dashboard
+    @user = current_user
+    @artworks = Artwork.where(user: @user)
+    @boookings = Booking.where(user: @user)
   end
 end
