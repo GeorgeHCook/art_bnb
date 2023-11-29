4ec959c3f01346ea8a4b2238e349bc7f78496124
Hannah Brett <hannahebrett89@icloud.com>
2023-11-29 10:47:34 +0000

    Updating models and index controller

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,8 +2,8 @@ class Artwork < ApplicationRecord
 
   belongs_to :user
   has_many :bookings
-  validates title, presence: true
-  validates description, presence: true
-  validates price_per_day, presence: true
-  validates category, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price_per_day, presence: true
+  validates :category, presence: true
 end
