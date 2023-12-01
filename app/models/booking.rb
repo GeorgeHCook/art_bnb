bac1f4270122348be5a24f2cb9eac1de4ca73c6a
George Cook <georgehhcook@gmail.com>
2023-12-01 13:20:11 +0000

    dashboar dcards done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :artwork
-  enum status: { pending: 0, processing_payment: 1, confirmed: 2, cancelled: -1 }
+  enum status: { pending: 0, processing_payment: 1, confirmed: 2, declined: -1 }
   # validates :start_date, presence: true
   # validates :end_date, presence: true
   # validates :status, presence: true
   # validates :user, presence: true, uniqueness: { scope: :artwork }
-  
+
 end
